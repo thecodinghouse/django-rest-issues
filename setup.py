89ac672c36c42cf102a3c41cb56b25473b1ab0bf
Gauravwagh <gauravwagh11@gmail.com>
2016-04-19 12:39:50 +0530

    added docs 1

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'django-rest-tickets',
+  packages = ['django-rest-tickets'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Django rest ticket is ticket raising system. Here logged in users can rais the issues.',
+  author = 'Gaurav Wagh',
+  author_email = 'waghgaurav.g@gmail.com',
+  url = 'https://github.com/Gauravwagh/django-rest-tickets', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/django-rest-tickets/tarball/0.1', # I'll explain this in a second
+  keywords = ['Django', 'Django-tickets', 'python'], # arbitrary keywords
+  classifiers = [],
+)
