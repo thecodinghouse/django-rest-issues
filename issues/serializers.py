b5afc07622c154b9a4cddba6689684fd99ed615a
Gauravwagh <gauravwagh11@gmail.com>
2016-04-19 15:06:48 +0530

    bug fixes for user serializer

--- issues/serializers.py
+++ issues/serializers.py
@@ -1,16 +1,21 @@
 # Django related imports
 from django.db import transaction
+from django.contrib.auth.models import User
 
 # Rest framework related imports
-from rest_auth.registration.serializers import slugify_unicode, unique_slug
 from rest_framework import serializers
 from rest_framework import filters
 
 # Local app related imports
 from .models import Issues, increment_issue_number
-from consultants.serializers import USER_MODELSerializer
 
+class USER_MODELSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False, style={'input_type': 'password'})
 
+    class Meta:
+        model = User
+
+        fields = ('id', 'first_name', 'last_name', 'email', 'password', 'username', 'is_staff', 'is_active')
 
 class IssuesSerializer(serializers.ModelSerializer):
     issue_owner = USER_MODELSerializer(required=False)
